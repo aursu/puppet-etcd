7f2d433fb61db72bf30b58b5536ec76b27ca89a5
Alexander Ursu <alexander.ursu@gmail.com>
2020-01-27 13:46:53 +0100

    Added etcd_initial_cluster_token parameter

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,23 +7,23 @@
 # ----------
 # [*etcd_version*]
 #   The version of etcd that you would like to use.
-#   Defaults to 3.2.18
+#   Defaults to 3.3.18
 #
 # [*etcd_archive*]
-#  The name of the etcd archive
-#  Defaults to etcd-v${etcd_version}-linux-amd64.tar.gz
+#   The name of the etcd archive
+#   Defaults to etcd-v${etcd_version}-linux-amd64.tar.gz
 #
 # [*etcd_source*]
-#  The URL to download the etcd archive
-#  Defaults to https://github.com/coreos/etcd/releases/download/v${etcd_version}/${etcd_archive}
+#   The URL to download the etcd archive
+#   Defaults to https://github.com/coreos/etcd/releases/download/v${etcd_version}/${etcd_archive}
 #
 # [*etcd_install_method*]
-#  The method on how to install etcd. Can be either wget (using etcd_source) or package (using $etcd_package_name)
-#  Defaults to wget
+#   The method on how to install etcd. Can be either wget (using etcd_source) or package (using $etcd_package_name)
+#   Defaults to wget
 #
 # [*etcd_package_name*]
-#  The system package name for installing etcd
-#  Defaults to etcd-server
+#   The system package name for installing etcd
+#   Defaults to etcd-server
 #
 # [*etcd_hostname*]
 #   The name of the etcd instance.
@@ -37,14 +37,18 @@
 #   Defaults to undef
 #
 # [*etcd_initial_cluster*]
-#    This will tell etcd how many nodes will be in the cluster and is passed as a string.
+#   This will tell etcd how many nodes will be in the cluster and is passed as a string.
 #   An example with hiera would be kubernetes::etcd_initial_cluster: etcd-kube-master=http://172.17.10.101:2380,etcd-kube-replica-master-01=http://172.17.10.210:2380,etcd-kube-replica-master-02=http://172.17.10.220:2380
 #   Defaults to undef
 #
 # [*etcd_initial_cluster_state*]
-#     This will tell etcd the initial state of the cluster. Useful for adding a node to the cluster. Allowed values are
+#   This will tell etcd the initial state of the cluster. Useful for adding a node to the cluster. Allowed values are
 #   "new" or "existing"
 #   Defaults to "new"
+#
+# [*etcd_initial_cluster_token*]
+#   Initial cluster token for the etcd cluster during bootstrap.
+#   Defaults to "my-etcd-token"
 #
 # [*etcd_ca_key*]
 #   This is the ca certificate key data for the etcd cluster. This must be passed as string not as a file.
@@ -52,14 +56,6 @@
 #
 # [*etcd_ca_crt*]
 #   This is the ca certificate data for the etcd cluster. This must be passed as string not as a file.
-#   Defaults to undef
-#
-# [*etcdclient_key*]
-#   This is the client certificate key data for the etcd cluster. This must be passed as string not as a file.
-#   Defaults to undef
-#
-# [*etcdclient_crt*]
-#   This is the client certificate data for the etcd cluster. This must be passed as string not as a file.
 #   Defaults to undef
 #
 # [*etcdserver_key*]
@@ -79,6 +75,7 @@
 #   Defaults to undef
 #
 class etcd (
+  String  $etcd_initial_cluster_token = 'my-etcd-token',
   Optional[String]
           $etcd_version               = '3.3.18',
   Optional[String]
@@ -89,10 +86,10 @@ class etcd (
           $etcd_initial_cluster       = undef,
   Optional[Enum['new', 'existing']]
           $etcd_initial_cluster_state = 'new',
-  String  $etcd_ca_key                = undef,
-  String  $etcd_ca_crt                = undef,
-  String  $etcdclient_key             = undef,
-  String  $etcdclient_crt             = undef,
+  Optional[String]
+          $etcd_ca_key                = undef,
+  Optional[String]
+          $etcd_ca_crt                = undef,
   Optional[String]
           $etcdserver_crt             = undef,
   Optional[String]
