1bd5e337eaa6f1f0a4539d3e4b4a9e8046f50520
Alexander Ursu <alexander.ursu@gmail.com>
2020-01-27 13:50:17 +0100

    Added servertype openstack for unit testing

--- spec/classes/etcd_spec.rb
+++ spec/classes/etcd_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'etcd' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
-      let(:facts) { os_facts }
+      let(:facts) { os_facts.merge(stype: 'openstack') }
 
       it { is_expected.to compile }
     end
--- spec/classes/packages_spec.rb
+++ spec/classes/packages_spec.rb
@@ -1,9 +1,11 @@
 require 'spec_helper'
 
 describe 'etcd::packages' do
+  let(:pre_condition) { 'include etcd' }
+
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
-      let(:facts) { os_facts }
+      let(:facts) { os_facts.merge(stype: 'openstack') }
 
       it { is_expected.to compile }
     end
