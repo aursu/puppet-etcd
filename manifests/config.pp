e613196edbf59c76ea15676eaea1bbd0180f05df
Alexander Ursu <alexander.ursu@gmail.com>
2020-01-27 13:47:54 +0100

    Set TLS settings optional

--- manifests/config.pp
+++ manifests/config.pp
@@ -6,23 +6,36 @@
 #   include etcd::config
 class etcd::config (
   String  $etcd_install_method        = $etcd::etcd_install_method,
-  String  $etcd_ca_key                = $etcd::etcd_ca_key,
-  String  $etcd_ca_crt                = $etcd::etcd_ca_crt,
-  String  $etcdclient_key             = $etcd::etcdclient_key,
-  String  $etcdclient_crt             = $etcd::etcdclient_crt,
-  String  $etcdserver_crt             = $etcd::etcdserver_crt,
-  String  $etcdserver_key             = $etcd::etcdserver_key,
-  String  $etcdpeer_crt               = $etcd::etcdpeer_crt,
-  String  $etcdpeer_key               = $etcd::etcdpeer_key,
+  Optional[String]
+          $etcd_ca_key                = $etcd::etcd_ca_key,
+  Optional[String]
+          $etcd_ca_crt                = $etcd::etcd_ca_crt,
+  Optional[String]
+          $etcdserver_crt             = $etcd::etcdserver_crt,
+  Optional[String]
+          $etcdserver_key             = $etcd::etcdserver_key,
+  Optional[String]
+          $etcdpeer_crt               = $etcd::etcdpeer_crt,
+  Optional[String]
+          $etcdpeer_key               = $etcd::etcdpeer_key,
   String  $etcd_hostname              = $etcd::etcd_hostname,
   String  $etcd_ip                    = $etcd::etcd_ip,
   String  $etcd_initial_cluster       = $etcd::etcd_initial_cluster,
   String  $etcd_initial_cluster_state = $etcd::etcd_initial_cluster_state,
+  String  $etcd_initial_cluster_token = $etcd::etcd_initial_cluster_token,
   String  $etcd_version               = $etcd::etcd_version,
 )
 {
   $kube_dirs = ['/etc/kubernetes', '/etc/kubernetes/pki', '/etc/kubernetes/pki/etcd']
-  $etcd = ['ca.crt', 'ca.key', 'client.crt', 'client.key', 'peer.crt', 'peer.key', 'server.crt', 'server.key']
+
+  $etcd = {
+    'ca.crt'     => $etcd_ca_crt,
+    'ca.key'     => $etcd_ca_key,
+    'peer.crt'   => $etcdpeer_crt,
+    'peer.key'   => $etcdpeer_key,
+    'server.crt' => $etcdserver_crt,
+    'server.key' => $etcdserver_key
+  }
 
   $kube_dirs.each | String $dir |  {
     file  { $dir :
@@ -32,11 +45,13 @@ class etcd::config (
     }
   }
 
-  $etcd.each | String $etcd_files | {
-    file { "/etc/kubernetes/pki/etcd/${etcd_files}":
-      ensure  => present,
-      content => template("etcd/${etcd_files}.erb"),
-      mode    => '0600',
+  $etcd.each | String $etcd_files, $content_string | {
+    if $content_string {
+      file { "/etc/kubernetes/pki/etcd/${etcd_files}":
+        ensure  => present,
+        content => template("etcd/${etcd_files}.erb"),
+        mode    => '0600',
+      }
     }
   }
 
