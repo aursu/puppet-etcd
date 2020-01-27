eab371f2eadc2c3e9fdad3e1338083391d306ab5
Alexander Ursu <alexander.ursu@gmail.com>
2020-01-27 13:50:51 +0100

    Covered default settings with unit tests

--- spec/classes/config_spec.rb
+++ spec/classes/config_spec.rb
@@ -1,11 +1,67 @@
 require 'spec_helper'
 
 describe 'etcd::config' do
+  let(:pre_condition) { 'include etcd' }
+
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
-      let(:facts) { os_facts }
+      let(:facts) do
+        os_facts.merge(
+          stype: 'openstack',
+          hostname: 'controller',
+        )
+      end
+
+      kube_dirs = ['/etc/kubernetes', '/etc/kubernetes/pki', '/etc/kubernetes/pki/etcd']
+      etcd = ['ca.crt', 'ca.key', 'peer.crt', 'peer.key', 'server.crt', 'server.key']
 
       it { is_expected.to compile }
+
+      kube_dirs.each do |d|
+        it {
+          is_expected.to contain_file(d.to_s)
+            .with_ensure('directory')
+        }
+      end
+
+      etcd.each do |f|
+        it { is_expected.not_to contain_file("/etc/kubernetes/pki/etcd/#{f}") }
+      end
+
+      it {
+        is_expected.to contain_file('/etc/systemd/system/etcd.service')
+          .with_content(%r{^ExecStart=/usr/local/bin/etcd --name controller})
+      }
+
+      it {
+        is_expected.to contain_file('/etc/systemd/system/etcd.service')
+          .with_content(%r{--listen-client-urls http://10.0.0.11:2379})
+      }
+
+      it {
+        is_expected.to contain_file('/etc/systemd/system/etcd.service')
+          .with_content(%r{--listen-peer-urls http://10.0.0.11:2380})
+      }
+
+      it {
+        is_expected.to contain_file('/etc/systemd/system/etcd.service')
+          .with_content(%r{--initial-cluster controller=http://10.0.0.11:2380})
+      }
+
+      it {
+        is_expected.to contain_file('/etc/systemd/system/etcd.service')
+          .with_content(%r{--initial-cluster-token my-etcd-token})
+      }
+
+      it {
+        is_expected.to contain_file('/etc/systemd/system/etcd.service')
+          .with_content(%r{--initial-cluster-state new})
+      }
+
+      it {
+        is_expected.to contain_file('/etc/systemd/system/etcd.service')
+          .without_content(%r{--client-cert-auth})
+      }
     end
   end
 end
