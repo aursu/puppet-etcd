5cdc27071dd1d591cb8116ac11429599d168af4a
Alexander Ursu <alexander.ursu@gmail.com>
2023-10-09 09:26:56 +0200

    Moved directories into etcd::setup

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,15 +1,58 @@
-# @summary A short summary of the purpose of this class
+# @summary Setup systemd service configuration for ETCD
 #
-# A description of what this class does
+# Setup systemd service configuration for ETCD
+#
+# @param ca_crt
+#   Content of supplied CA certificate in PEM format. A shared cluster CA certificate (ca.crt) for
+#   both peer connections and client connections. This certificate will be stored
+#   into /etc/kubernetes/pki/etcd/ca.crt
+#
+# @param ca_key
+#   Optionally supplied CA certificate KEY content. This key will be stored
+#   into /etc/kubernetes/pki/etcd/ca.key
+#
+# @param server_crt
+#   Content of the certificate in PEM format used both encrypts traffic and authenticates its
+#   connections. This certificate will be stored into /etc/kubernetes/pki/etcd/server.crt
+#
+# @param server_key
+#   Content of the certificate KEY. Key for the certificate `server_crt`. Must be unencrypted.
+#   This KEY will be stored into /etc/kubernetes/pki/etcd/server.key
+#
+# @param peer_crt
+#   Content of the Peer certificate. Certificate used for SSL/TLS connections between peers.
+#   This will be used both for listening on the peer address as well as sending requests to other
+#   peers. This certificate will be copied into /etc/kubernetes/pki/etcd/peer.crt
+#
+# @param peer_key
+#   Content of the Peer certificate KEY. Key for the Peer certificate `peer_crt`. Must be
+#   unencrypted. This KEY will be copied into /etc/kubernetes/pki/etcd/peer.key
+#
+# @param ca_pki
+#   If set to ture then CA certificate has been already installed by user into
+#   /etc/kubernetes/pki/etcd/ca.crt
+#
+# @param server_pki
+#   If set to ture then clients' communication certificate and key have been already installed
+#   by user into /etc/kubernetes/pki/etcd/server.crt and /etc/kubernetes/pki/etcd/server.key
+#   accordingly
+#
+# @param peer_pki
+#   If set to ture then peers' communication certificate and key have been already installed
+#   by user into /etc/kubernetes/pki/etcd/peer.crt and /etc/kubernetes/pki/etcd/peer.key
+#   accordingly
 #
 # @example
 #   include etcd::config
 class etcd::config (
   String $install_method = $etcd::install_method,
+  Boolean $ca_pki = false,
   Optional[String] $ca_key = $etcd::ca_key,
   Optional[String] $ca_crt = $etcd::ca_crt,
+  Boolean $server_pki = false,
   Optional[String] $server_crt = $etcd::server_crt,
   Optional[String] $server_key = $etcd::server_key,
+  Boolean $peer_pki = false,
   Optional[String] $peer_crt = $etcd::peer_crt,
   Optional[String] $peer_key = $etcd::peer_key,
   String $hostname = $etcd::hostname,
@@ -21,10 +64,7 @@ class etcd::config (
   Integer $snapshot_count = $etcd::snapshot_count,
 ) inherits etcd::globals {
   include bsys::systemctl::daemon_reload
-
-  $version = $etcd::globals::version
-
-  $kube_dirs = ['/etc/kubernetes', '/etc/kubernetes/pki', '/etc/kubernetes/pki/etcd']
+  include etcd::setup
 
   $etcd_pki = {
     'ca.crt'     => $ca_crt,
@@ -33,14 +73,6 @@ class etcd::config (
     'peer.key'   => $peer_key,
     'server.crt' => $server_crt,
     'server.key' => $server_key,
-  }
-
-  $kube_dirs.each | String $dir | {
-    file { $dir :
-      ensure  => directory,
-      mode    => '0600',
-      recurse => true,
-    }
   }
 
   $etcd_pki.each |String $pki_file, $content_string| {
